4cefef6c7bfc057d52eb52778909b06db94269b3
MohHilmanFariz <farizhilman6@gmail.com>
2024-06-20 00:41:20 +0700

    rename file php

--- app/View/Components/tvshowCard.php
+++ app/View/Components/tvshowCard.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class tvshow-card extends Component
+class tvshowCard extends Component
 {
     /**
      * Create a new component instance.
